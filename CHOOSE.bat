734dedd46f6cd8856f8373b596669ac36b8f633a
phasmagoria <depressed@national.shitposting.agency>
2021-03-11 05:32:21 -0500

    Update CHOOSE.bat

--- CHOOSE.bat
+++ CHOOSE.bat
@@ -1,10 +1,12 @@
 @ECHO OFF
 :BEGIN
 CLS
-ECHO 1 is for Bravery
-ECHO 2 is for Brilliance
-ECHO 3 is for Balance
-CHOICE /N /C:123 /M "(1, 2, or 3)"%1
+ECHO 1 is for Bravery.
+ECHO 2 is for Brilliance.
+ECHO 3 is for Balance.
+ECHO 4 is for leaving Hypesquad all together.
+CHOICE /N /C:1234 /M "(1, 2, 3 or 4)"%1
+IF ERRORLEVEL ==4 GOTO LEAVE
 IF ERRORLEVEL ==3 GOTO BALANCE
 IF ERRORLEVEL ==2 GOTO BRILLIANCE
 IF ERRORLEVEL ==1 GOTO BRAVERY
@@ -20,5 +22,8 @@ GOTO END
 :BRAVERY
 ECHO You should have the Bravery Hypesquad badge now.
 python bravery.py
+:LEAVE
+ECHO You should have left Hypesquad now.
+python leave.py
 :END
 pause
